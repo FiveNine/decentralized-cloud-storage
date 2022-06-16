9b1fc5fd539228587acb564f718cb28982eb2f2b
Muhammad Sarosh <20180289@stu.uob.edu.bh>
2022-06-16 15:41:04 +0300

    test peer-to-peer

--- Server/test_server.py
+++ Server/test_server.py
@@ -1,14 +1,20 @@
-import socket
+import socket, pickle
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind(('0.0.0.0', 59590))
         sock.listen(1)
 
-        sock, address = sock.accept()
-        print(f"{address} connected.")
-        print(sock.recv(1024).decode('utf-8'))
-        sock.sendall("world".encode('utf-8'))
+        sock_a, address_a = sock.accept()
+        print(f"{address_a} connected.")
+
+        sock_b, address_b = sock.accept()
+        print(f"{address_b} connected.")
+
+        sock_a.sendall(pickle.dumps(address_b))
+        sock_b.sendall(pickle.dumps(address_a))
+        print("Exchanged addresses, quitting now.")
+        input()
 
 
 if __name__ == '__main__':
