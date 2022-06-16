1436e868ecb8cf648238498eccd4a4b3c4d440ce
Muhammad Sarosh <20180289@stu.uob.edu.bh>
2022-06-16 15:22:20 +0300

    server test script

---
+++ Server/test_server.py
@@ -0,0 +1,15 @@
+import socket
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind(('0.0.0.0', 59590))
+        sock.listen(1)
+
+        sock, address = sock.accept()
+        print(address + " connected.")
+        print(sock.recv(1024).decode('utf-8'))
+        sock.sendall("world".encode('utf-8'))
+
+
+if __name__ == '__main__':
+    main()
