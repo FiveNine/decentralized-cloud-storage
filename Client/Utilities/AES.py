d3904c54466ea69aff88cfab98d30f0515a48acd
Muhammad Sarosh <20180289@stu.uob.edu.bh>
2022-06-24 21:46:34 +0300

    file division, encryption, decryption and merging

--- Client/Utilities/AES.py
+++ Client/Utilities/AES.py
@@ -1,66 +1,157 @@
-from cryptography.fernet import Fernet
-import os
+import os, io, json
+from base64 import b64encode, b64decode
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
-def generate_key(file_name: str) -> bytes:
-    key = Fernet.generate_key()
-    if not os.path.exists("./Keys"):
-        os.makedirs("./Keys")
-    with open(f"./Keys/{file_name}.key", 'wb') as key_file:
-        key_file.write(key)
-    return key
+def save_keys(save_directory: str, save_name: str, key: bytes, signature: bytes, initialization_vector: bytes, tag: bytes):
+    """
+    Save key, signature, initialization vector and tag in a file.\n
+    Arguments:
+        save_directory: Directory to save the keys in.
+        save_name: Name of the file to save the keys in (without extension).
+        key: Encryption Key generated on Encryption.
+        signature: Encryption Signature generated on Encryption.
+        initialization_vector: Initialization vector generated on Encryption.
+        tag: Tag generated on Encryption.
+    """
 
-def get_key(file_name: str) -> None:
-    try:
-        return open(f"./Keys/{file_name}.key", 'rb').read()
-    except FileNotFoundError:
-        print("Key file not found in Keys directory.")
+    # Convert all bytes to base64 and then decode using utf-8 
+    # to store them as strings in json
+    key = b64encode(key).decode('utf-8')
+    signature = b64encode(signature).decode('utf-8')
+    initialization_vector = b64encode(initialization_vector).decode('utf-8')
+    tag = b64encode(tag).decode('utf-8')
 
-def encrypt_file(file_path: str, key: bytes) -> str:
-    file_path = os.path.abspath(file_path)
-    identifier, extension = file_path.split('\\')[-1].split('.')
-
-    if not os.path.exists("./Encrypted"):
-        os.makedirs("./Encrypted")
-
-    with open(file_path, 'rb') as file_to_encrypt, open(f"./temp/{identifier}.{extension}", 'wb') as encrypted_file:
-        f = Fernet(key)
-        read_buffer = 50 * 1024 * 1024 # 50MB
-
-        while True:
-            data = file_to_encrypt.read(read_buffer)
-            if not data:
-                break
-            encrypted_data = f.encrypt(data)
-            encrypted_file.write(encrypted_data)
-    return f"./temp/{identifier}.encrypted"
-
-def decrypt_file(file_path: str, key: bytes) -> None:
-    file_path = os.path.abspath(file_path)
-    identifier, extension = file_path.split('\\')[-1].split('.')
-
-    if not os.path.exists("./Decrypted"):
-        os.makedirs("./Decrypted")
-
-    with open(file_path, 'rb') as file_to_decrypt, open(f"./Decrypted/{identifier}.{extension}", 'wb') as decrypted_file:
-        f = Fernet(key)
-        read_buffer = 50 * 1024 * 1024 # 50MB
-
-        while True:
-            data = file_to_decrypt.read(read_buffer)
-            if not data:
-                break
-            decrypted_data = f.decrypt(data)
-            decrypted_file.write(decrypted_data)
+    key_object = {
+        "Key": key,
+        "Signature": signature,
+        "IV": initialization_vector,
+        "Tag": tag
+    }
     
-# ----test----
-# generate_key("./test.file")
-# print("Key Generated")
+    file_name = save_name + ".key"
+    with open(os.path.join(save_directory, file_name), 'w') as file:
+        file.write(json.dumps(key_object, indent=4))
 
-# key = get_key("./test.file")
-# print(key)
+def get_keys(save_directory: str, save_name: str) -> tuple[bytes, bytes, bytes, bytes]:
+    """
+    Gets key, signature, initialization vector and tag stored in a file.\n
+    Arguments:
+        save_directory: Directory in which the save file is.
+        save_name: Name of the file with the keys (without extension).
+    Returns:
+        key: Encryption Key generated on Encryption.
+        signature: Encryption Signature generated on Encryption.
+        initialization_vector: Initialization vector generated on Encryption.
+        tag: Tag generated on Encryption.
+    """
 
-# encrypt_file("./test.file", key)
-# print("File Encrypted")
+    file_name = save_name + ".key"
 
-# decrypt_file("./Encrypted/test.file", key)
-# print("File Decrypted")
+    with open(os.path.join(save_directory, file_name), 'r') as file:
+        key_object = json.loads(file.read())
+
+    key, signature, initialization_vector, tag = key_object.values()
+
+    # Encode strings using utf-8 and use base64 decoding
+    # to get the original values in bytes
+    key = b64decode(key.encode('utf-8'))
+    signature = b64decode(signature.encode('utf-8'))
+    initialization_vector = b64decode(initialization_vector.encode('utf-8'))
+    tag = b64decode(tag.encode('utf-8'))
+
+    return (key, signature, initialization_vector, tag)
+
+def iter_chunks(file: io.BytesIO, chunk_size: int = 64 * 1024):
+    """Read chunks from a file"""
+    while True:
+        data = file.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+class EncryptionIterator:
+    """
+    Encrypt a file iteratively
+    Parameters
+    ----------
+    plaintext : io.BytesIO
+        The file buffer to encrypt
+    chunk_size : int
+        How much data to encrypt per iteration
+    Attributes
+    ----------
+    key : bytes
+        The secret key for AES encryption
+    signature : bytes
+        Additional data used to verify the key for dec
+    iv : bytes
+        The 12-byte initialization vector for GCM. You will need this value for decryption.
+    tag : bytes
+        The tag to verfiy data integrity on decryption
+    """
+
+    def __init__(
+        self,
+        plaintext: io.BytesIO,
+        chunk_size: int = 64 * 1024,
+    ):
+        self.key = os.urandom(32)
+        self.signature = os.urandom(12)
+        self.iv = os.urandom(12)
+        self.file = plaintext
+        self.chunk_size = chunk_size
+        self.encryptor = Cipher(algorithms.AES(self.key), modes.GCM(self.iv)).encryptor()
+        self.encryptor.authenticate_additional_data(self.signature)
+
+    @property
+    def tag(self):
+        return self.encryptor.tag
+
+    def __iter__(self):
+        for chunk in iter_chunks(self.file, chunk_size=self.chunk_size):
+            yield self.encryptor.update(chunk)
+
+        yield self.encryptor.finalize()
+
+class DecryptionIterator:
+    """
+    Decrypt a file iteratively
+    Parameters
+    ----------
+    ciphertext : io.BytesIO
+        The file buffer to decrypt
+    key : bytes
+        The secret key for AES decryption
+    signature : bytes
+        Additional data used to verify the key
+    iv : bytes
+        The initialization vector from the EncryptionIterator object
+    tag : bytes
+        The tag used for data integrity from the EncryptionIterator object
+    chunk_size : int
+        How much data to decrypt per iteration
+    """
+
+    def __init__(
+        self,
+        ciphertext: io.BytesIO,
+        key: bytes,
+        signature: bytes,
+        iv: bytes,
+        tag: bytes,
+        chunk_size: int = 64 * 1024,
+    ):
+        self.iv = iv
+        self.tag = tag
+        self.file = ciphertext
+        self.chunk_size = chunk_size
+        self.decryptor = Cipher(
+            algorithms.AES(key), modes.GCM(self.iv, self.tag)
+        ).decryptor()
+        self.decryptor.authenticate_additional_data(signature)
+
+    def __iter__(self):
+        for chunk in iter_chunks(self.file, chunk_size=self.chunk_size):
+            yield self.decryptor.update(chunk)
+
+        yield self.decryptor.finalize()
--- Client/Utilities/utils.py
+++ Client/Utilities/utils.py
@@ -1,53 +1,126 @@
 import os, math
 import Utilities.AES as AES
 
-def divide_file(full_path: str) -> list[str]:
-    full_path = os.path.abspath(full_path)
-    identifier, extension = full_path.split('\\')[-1].split('.')
+GIGA_BYTE = 1024 * 1024 * 1024
+MEGA_BYTE = 1024 * 1024
+KILO_BYTE = 1024
 
-    key = AES.generate_key(identifier)
-    encrypted_file_path = AES.encrypt_file(full_path, key)
+def divide_file(file_path: str) -> list[str]:
+    """
+    Encrypt the file using AES and divide it into 10 chunks.\n
+    Arguments:
+        file_path: Absolute path to the file to be divided.
+    Return:
+        List of paths to the encrypted and divided files.
+    """
+    file_path = os.path.abspath(file_path)
+    identifier, extension = file_path.split('\\')[-1].split('.')
+    file_size = os.path.getsize(file_path)
 
-    total_size = os.path.getsize(encrypted_file_path) # in bytes
+    # create temp folder to save encrypted files
+    if not os.path.exists("./temp"):
+        os.makedirs("./temp")
+
+    # encrypt file
+    plaintext_file = open(file_path, 'rb')
+    encrypted_file_path = f"./temp/{identifier}.{extension}"
+    encryptor = AES.EncryptionIterator(plaintext_file, min(math.ceil(file_size/4), 100 * MEGA_BYTE))
+    print(encryptor.chunk_size)
+    with open(encrypted_file_path, 'wb') as encrypted_file:
+        for chunk in encryptor:
+            encrypted_file.write(chunk)
+    plaintext_file.close()
+
+    # save encryption keys
+    if not os.path.exists("./Keys"):
+        os.makedirs("./Keys")
+    AES.save_keys("./Keys", identifier, encryptor.key, encryptor.signature, encryptor.iv, encryptor.tag)
+
+    # divide encrypted file
+    total_size = os.path.getsize(encrypted_file_path)
     divisions = 10
     chunk_size = math.ceil(total_size / divisions)
-    prev_pos = 0
+
+    # Buffer size of chunk_size/4 allows smaller files 
+    # to be divided evenly into 10 files. Tested for 
+    # files greater than 2MB. Buffer size will be 100MB max.
+    read_buffer = min(math.ceil(chunk_size/4), 100 * MEGA_BYTE)
     
-    for i in range(0, divisions):
-        with open(encrypted_file_path, 'rb') as read_file, open(f'./temp/{identifier}{i}.{extension}', 'wb') as write_file:
-            data_read_in_bytes = 0
+    list_of_files: list[str] = []
+    with open(encrypted_file_path, 'rb') as read_file:
+        for i in range(0, divisions):
+            with open(f'./temp/{identifier}{i}.{extension}', 'wb') as write_file:
 
-            # Buffer size of chunk_size/4 allows smaller files 
-            # to be divided evenly into 10 files. Tested for 
-            # files greater than 2MB. Buffer size will be 100MB max.
-            read_buffer = min(math.ceil(chunk_size/4), 100 * 1024 * 1024)
-            read_file.seek(prev_pos)
+                data_read_in_bytes = 0
+                while True:
+                    data = read_file.read(read_buffer)
+                    if not data:
+                        break
 
-            while True:
-                data = read_file.read(read_buffer)
-                if not data:
-                    prev_pos = read_file.tell()
-                    break
-
-                data_read_in_bytes += len(data)
-                write_file.write(data)
-                if data_read_in_bytes > chunk_size:
-                    prev_pos = read_file.tell()
-                    break
+                    data_read_in_bytes += len(data)
+                    write_file.write(data)
+                    if data_read_in_bytes >= chunk_size:
+                        break
+                list_of_files.append(os.path.abspath(f'./temp/{identifier}{i}.{extension}'))
     
-    return os.listdir('./temp/')
+    return list_of_files
+
+def restore_file(list_of_files: list[str]) -> str:
+    identifier, extension = list_of_files[0].split('\\')[-1].split('.')
+    identifier = identifier[0:-1]
+
+    if not os.path.exists("./Downloads/temp"):
+        os.makedirs("./Downloads/temp")
+
+    merged_file_path = f'./Downloads/temp/{identifier}.{extension}'
+
+    with open(merged_file_path, 'wb') as write_file:
+        for file_path in list_of_files:
+            with open(file_path, 'rb') as read_file:
+                file_size = os.path.getsize(file_path)
+                read_buffer = min(math.ceil(file_size/4), 100 * MEGA_BYTE)
+
+                data_read = 0
+                while True:
+                    data = read_file.read(read_buffer)
+                    if not data:
+                        break
+                    data_read += len(data)
+                    write_file.write(data)
+                    if data_read > file_size:
+                        break
+    
+    decrypted_file_path = f"./Downloads/{identifier}.{extension}"
+    ciphertext_file_size = os.path.getsize(merged_file_path)
+    ciphertext_file = open(merged_file_path, 'rb')
+
+    key, signature, iv, tag = AES.get_keys("./Keys", identifier)
+    decryptor = AES.DecryptionIterator(
+        ciphertext= ciphertext_file,
+        key= key,
+        signature= signature,
+        iv= iv,
+        tag= tag,
+        chunk_size= min(math.ceil(ciphertext_file_size/4), 100 * MEGA_BYTE)
+    )
+    print(decryptor.chunk_size)
+    with open(decrypted_file_path, 'wb') as decrypted_file:
+        for chunk in decryptor:
+            decrypted_file.write(chunk)
+    ciphertext_file.close()
+    
+    delete_directory("./Downloads/temp/")
+
+    return decrypted_file_path
 
 def delete_directory(path: str) -> None:
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                delete_directory(file_path)
-                os.rmdir(file_path)
-        except Exception as e:
-            print(e)
+    for file_name in os.listdir(path):
+        file = path + file_name
+        if os.path.isfile(file):
+            os.unlink(file)
+        elif os.path.isdir(file):
+            delete_directory(file)
+    os.rmdir(path)
 
 def address_to_string(address: tuple[str, int]) -> str:
     return address[0] + ':' + str(address[1])
@@ -55,6 +128,3 @@ def address_to_string(address: tuple[str, int]) -> str:
 def string_to_address(string: str) -> tuple[str, int]:
     host, port = string.split(':')
     return host, int(port)
-
-# def merge_files()
-
